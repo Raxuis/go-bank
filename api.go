c517deb9d19535dc3d6ebd57f29c46555c735752
Raxuis <raclot.raphael@gmail.com>
2024-05-31 19:02:02 +0200

    feat(write json): :sparkles: add writeJSON function

--- api.go
+++ api.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
 
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
